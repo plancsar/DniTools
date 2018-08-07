95f6a4fad44602f97f891a412b8b2b07508aed6d
plancsar <plancsar@users.noreply.github.com>
2018-08-07 17:53:06 +0200

    D'ni->decimal also spells the number in D'ni

--- dninum.py
+++ dninum.py
@@ -9,7 +9,9 @@ import sys
 
 parser = argparse.ArgumentParser(description="""
 Converts numbers between base 10 and 25, with various formats. The default
-format uses brackets, e.g. [11|12|6].
+format uses brackets, e.g. [11|12|6]. For the D'ni to decimal conversion, 
+the integer part of the result is also spelled in D'ni, as long as it is 
+below 6 digits.
 Algorithm based on: "The math behind converting from any base to any base
 without going through base 10" (http://is.gd/aa9xJb).
 
@@ -41,87 +43,112 @@ args = parser.parse_args()
 dniAlphaDigitsDni = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,\
                      "A":10,"B":11,"C":12,"D":13,"E":14,"F":15,"G":16,"H":17,\
                      "I":18,"J":19,"K":20,"L":21,"M":22, "N":23,"O":24,".":25}
-                     
+
 dniFontDigitsDni  = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,
                      "\)":10,"!":11,"@":12,"#":13,"$":14,"%":15,"^":16,"&":17,\
                      "*":18,"\(":19,"[":20,"]":21,"\\":22,"\{":23,"\}":24,".":25}
 
 dniAlphaDigitsDec = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E",\
                      "F","G","H","I","J","K","L","M", "N","O","."]
-                     
+
 dniFontDigitsDec  = ["0","1","2","3","4","5","6","7","8","9",")","!","@","#","$",\
                      "%","^","&","*","(","[","]","\\","{","}","."]
 
+dniDigitsSpellOts = ["roon","fah","bree","sehn","tor","vaht","vahgahfah","vahgahbree","vahgahsen","vahgahtor","nayvoo","naygahfah","naygahbree","naygahsen","naygahtor","heebor","heegahfah","heegahbree","heegahsen","heegahtor","rish","rigahfah","rigahbree","rigahsen","rigahtor","."]
+
+dniDigitsSpellNts = ["rún","fa","brí","sen","tor","vat","vagafa","vagabrí","vagasen","vagator","névú","négafa","négabrí","négasen","négator","híbor","hígafa","hígabrí","hígasen","hígator","riš","rigafa","rigabrí","rigasen","rigator","."]
+
+dniDigitsPowers = ["","see","rah","lahn","mel","blo"]
+
 
 def dec2dni(dec):
-	dni = []
-	dec = math.fabs(dec)
-	dec_i = int(dec)
-	dec_d = dec - dec_i
-	cont = 0
-	while dec_i > 0 :
-		dni.insert(0, dec_i % 25)
-		dec_i  = dec_i // 25
-	dni.insert(len(dni),25)
-	while dec_d > 0 :
-		dec_25 = dec_d * 25
-		dec_d = dec_25 - int(dec_25)
-		dni.insert(len(dni), int(dec_25))
-		cont = cont + 1
-		if cont > 6 :
-			break
-	return dni
+    dni = []
+    dec = math.fabs(dec)
+    dec_i = int(dec)
+    dec_d = dec - dec_i
+    cont = 0
+    while dec_i > 0 :
+        dni.insert(0, dec_i % 25)
+        dec_i  = dec_i // 25
+    dni.insert(len(dni),25)
+    while dec_d > 0 :
+        dec_25 = dec_d * 25
+        dec_d = dec_25 - int(dec_25)
+        dni.insert(len(dni), int(dec_25))
+        cont = cont + 1
+        if cont > 6 :
+            break
+
+    return dni
 
 def dni2dec(dni):
-	dec = 0
-	dot = 0
-	for d in dni :
-		if dot == 0 :
-			if d == 25 :
-				dot = 1
-			else:
-				dec = 25 * dec + d
-		else:
-			dec = (d / 25**dot) + dec 
-			dot = dot + 1
-		
-	return dec
+    dec = 0
+    dot = 0
+    for d in dni :
+        if dot == 0 :
+            if d == 25 :
+                dot = 1
+            else:
+                dec = 25 * dec + d
+        else:
+            dec = (d / 25**dot) + dec
+            dot = dot + 1
+
+    return dec
 
 
 if args.dec:
-	resultB = dec2dni(args.dec)
-	result = []
-	if args.font:
-		for i in range(0,len(resultB)):
-			result.append(dniFontDigitsDec[resultB[i]])
-		print('base25: '+''.join(map(str,result)))
-	elif args.alpha:
-		for i in range(0,len(resultB)):
-			result.append(dniAlphaDigitsDec[resultB[i]])
-		print('base25: '+''.join(map(str,result)))
-	else:
-		for i in range(0,len(resultB)):
-			if resultB[i] == 25: resultB[i] = "."
-		print('base25: ['+'|'.join(map(str,resultB))+']')
+    resultB = dec2dni(args.dec)
+    result = []
+    spell_count = 0
+    spell_text = []
+    for i in range(0,len(resultB)):
+        if resultB[i] == 25:
+            break
+        spell_count += 1
+
+    if args.font:
+        for i in range(0,len(resultB)):
+            result.append(dniFontDigitsDec[resultB[i]])
+        print('base25: '+''.join(map(str,result)))
+    elif args.alpha:
+        for i in range(0,len(resultB)):
+            result.append(dniAlphaDigitsDec[resultB[i]])
+        print('base25: '+''.join(map(str,result)))
+    else:
+        for i in range(0,len(resultB)):
+            if resultB[i] == 25: resultB[i] = "."
+        print('base25: ['+'|'.join(map(str,resultB))+']')
+
+    if spell_count < 7:
+        for i in range(2,spell_count+2):
+            if dniDigitsSpellOts[resultB[-i]] != "roon":
+                spell_text.append(dniDigitsSpellOts[resultB[-i]] + dniDigitsPowers[i-2] + " ")
+
+        spell_str = "".join(map(str,spell_text[::-1]))
+        spell_str = spell_str.replace("torrah", "torah")
+        print(spell_str)
+
 
 elif args.dni:
-	input = []
-	if args.font:
-		print "This option is bugged, even if you get a result it will likely be wrong!"
-		for i in range(0,len(args.dni)):
-			input.append(dniFontDigitsDni[args.dni[i]])
-		result = dni2dec(input)
-		print "base10: %f" % (result)
-	elif args.alpha:
-		for i in range(0,len(args.dni)):
-			input.append(dniAlphaDigitsDni[args.dni[i].upper()])
-		result = dni2dec(input)
-		print "base10: %f" % (result)
-	else:
-		for i in range(0,len(args.dni)):
-			if args.dni[i] == ".":
-				args.dni[i] = 25
-			elif int(args.dni[i]) > 24:
-				sys.exit("At least one digit is too big!")
-		result = dni2dec(map(float,args.dni))
-		print "base10: %f" % (result)
+    input = []
+    if args.font:
+        print("This option is bugged, even if you get a result it will likely be wrong!")
+        for i in range(0,len(args.dni)):
+            input.append(dniFontDigitsDni[args.dni[i]])
+        result = dni2dec(input)
+        print "base10: %f" % (result)
+    elif args.alpha:
+        for i in range(0,len(args.dni)):
+            input.append(dniAlphaDigitsDni[args.dni[i].upper()])
+        result = dni2dec(input)
+        print "base10: %f" % (result)
+    else:
+        for i in range(0,len(args.dni)):
+            if args.dni[i] == ".":
+                args.dni[i] = 25
+            elif int(args.dni[i]) > 24:
+                sys.exit("At least one digit is too big!")
+        result = dni2dec(map(float,args.dni))
+        print "base10: %f" % (result)
+
