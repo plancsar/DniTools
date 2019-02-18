01f07a510bbd9fbcb2cad36e9187fe7b844c9049
plancsar <plancsar@users.noreply.github.com>
2019-02-18 10:40:23 +0100

    Update dniclock.sh

--- dniclock.sh
+++ dniclock.sh
@@ -2,7 +2,7 @@
 # Use the terminal window as a rudimentary D'ni clock
 # Exit with ctrl-c
 
-printf '\e[8;3;33t'      # resize the terminal window to 3 lines x 33 columns
+printf '\e[8;3;40t'      # resize the terminal window to 3 lines x 40 columns
 printf '\e[3;0;0t'       # move the window to the top/left corner of the display
 printf '\e[2J'           # clear screen
 while [ 1 ]; do
