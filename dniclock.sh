a4db4928416f7889e5a4dc3bd92fbe3a5107ac35
plancsar <plancsar@users.noreply.github.com>
2019-02-18 10:39:42 +0100

    Update dniclock.sh

--- dniclock.sh
+++ dniclock.sh
@@ -2,11 +2,11 @@
 # Use the terminal window as a rudimentary D'ni clock
 # Exit with ctrl-c
 
-printf '\e[8;3;40t'      # resize the terminal window to 3 lines x 40 columns
+printf '\e[8;3;33t'      # resize the terminal window to 3 lines x 33 columns
 printf '\e[3;0;0t'       # move the window to the top/left corner of the display
-printf '\033[2J'         # clear screen
+printf '\e[2J'           # clear screen
 while [ 1 ]; do
-	printf '\033[0;0H\n'  # move to first line
-	./dnitime.py -n
-	sleep 1
+    printf '\e[0;0H\n'   # move to first line
+    echo "  $(dnitime.py -p)"
+    sleep 1
 done
